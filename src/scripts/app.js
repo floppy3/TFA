873dd40d47a7b3e83a1b25bacd8939f6633fd2f2
floppy3 <flolambert@live.fr>
2023-05-30 17:17:15 +0200

    version 1 burger menu +responsive

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,14 @@
-console.info('Hello world');
+"use strict" ;
+
+let menuBurger = document.querySelector('.menu__burger');
+menuBurger.addEventListener('click',toggleNavigation);
+
+function toggleNavigation(){
+    if(document.body.hasAttribute("data-menu")){
+        document.body.removeAttribute("data-menu");
+        menuBurger.innerHTML = "Menu";
+    }else{
+        document.body.setAttribute("data-menu", true);
+        menuBurger.innerHTML = "Close";
+    }
+}
