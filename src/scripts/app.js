b720390784ea3713ef5b02807c2079df2b8bc87c
floppy3 <flolambert@live.fr>
2023-05-31 13:50:36 +0200

    ancre togglenav + smoothscroll

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,8 @@
 
 let menuBurger = document.querySelector('.menu__burger');
 menuBurger.addEventListener('click',toggleNavigation);
+let navLinks = document.querySelectorAll('.menu__el a');
+navLinks.forEach(link => link.addEventListener('click', toggleNavigation));
 
 function toggleNavigation(){
     if(document.body.hasAttribute("data-menu")){
