4bcb4fdf8cfdc403a0af7687faa7f7b928f03016
floppy3 <flolambert@live.fr>
2023-06-09 17:15:36 +0200

    nav anim et burger menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,16 +6,17 @@ gsap.registerPlugin(ScrollTrigger);
 /* Burger menu */
 let menuBurger = document.querySelector('.menu__burger');
 menuBurger.addEventListener('click',toggleNavigation);
+let menuSpan = document.querySelector('.menu__burger span');
 let navLinks = document.querySelectorAll('.menu__el a');
 navLinks.forEach(link => link.addEventListener('click', toggleNavigation));
 
 function toggleNavigation(){
     if(document.body.hasAttribute("data-menu")){
         document.body.removeAttribute("data-menu");
-        menuBurger.innerHTML = "Menu";
+        menuSpan.innerHTML = "Menu";
     }else{
         document.body.setAttribute("data-menu", true);
-        menuBurger.innerHTML = "Close";
+        menuSpan.innerHTML = "Close";
     }
 }
 
